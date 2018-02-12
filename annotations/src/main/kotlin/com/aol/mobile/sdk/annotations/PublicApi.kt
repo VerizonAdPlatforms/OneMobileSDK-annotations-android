9d9e908a146f534eda7160af4d8f5d024388db63
Alexander Levadniy <aleksander.levadniy@teamaol.com>
2018-02-12 16:31:08 +0200

    Private api annotation

--- annotations/src/main/kotlin/com/aol/mobile/sdk/annotations/PublicApi.kt
+++ annotations/src/main/kotlin/com/aol/mobile/sdk/annotations/PublicApi.kt
@@ -22,4 +22,8 @@ package com.aol.mobile.sdk.annotations
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class PublicApi(val pkg: String)
+annotation class PublicApi
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class PrivateApi
